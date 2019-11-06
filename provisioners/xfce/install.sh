8e1ae3d15ceb95298427ce368fb14167bbf40c99
Joshua Makinen <makinj64@gmail.com>
2019-11-05 16:48:24 -0800

    adds random background generation script

--- provisioners/xfce/install.sh
+++ provisioners/xfce/install.sh
@@ -12,6 +12,8 @@ if [ ! -f "$once" ]; then
   echo 'if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then exec startx; fi' >> "$HOME/.bash_profile"
   touch "$once";
 fi
+wget https://picsum.photos/1920/1200 -O /usr/share/backgrounds/xfce/xfce-teal.jpg
+chmod 644 /usr/share/backgrounds/xfce/xfce-teal.jpg
 
 if [ -f "$configtar" ]; then
   tar -xzf "$configtar" -C /
