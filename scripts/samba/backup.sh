f8f8833500b5f2210cfe22d64e51bee5366e46ae
Joshua Makinen <makinj64@gmail.com>
2019-11-14 22:10:08 -0800

    tries to get password db import/export working

--- scripts/samba/backup.sh
+++ scripts/samba/backup.sh
@@ -4,12 +4,11 @@ nonce="$(head -c 10 /dev/urandom | base32)"
 
 
 
-vagrant ssh -c "sudo pdbedit -e smbpasswd:/tmp/users.backup.tdb;"
+#vagrant ssh -c "sudo pdbedit -e smbpasswd:/tmp/users.backup.tdb;"
 vagrant ssh -c "tar -C  / -cf '/tmp/config-$nonce.tar' \"/etc/samba/\";"
-vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/tmp/users.backup.tdb\";"
+#vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/tmp/users.backup.tdb\";"
 vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/etc/passwd\";"
-#vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/var/lib/samba/\";"
+vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/etc/group\";"
+vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/var/lib/samba/\";"
 vagrant ssh -c "gzip '/tmp/config-$nonce.tar';"
 vagrant scp "default:/tmp/config-$nonce.tar.gz" "$1"
-
-
