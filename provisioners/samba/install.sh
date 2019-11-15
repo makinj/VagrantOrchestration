2881234d9be321a1f96e44cdc8d3a937e5962cfe
Joshua Makinen <makinj64@gmail.com>
2019-11-14 21:43:06 -0800

    tries to get password db import/export working

--- provisioners/samba/install.sh
+++ provisioners/samba/install.sh
@@ -7,6 +7,9 @@ configtar="$HOME/configs/samba/config.tar.gz"
 
 if [ -f "$configtar" ]; then
   sudo tar -xzf "$configtar" -C /
+  if [ -f "/tmp/users.backup.tdb" ]; then
+    sudo pdbedit -i smbpasswd:/tmp/users.backup.tdb
+  fi
 fi
 
 sudo cp $HOME/configs/samba/smb.conf /etc/samba/smb.conf
