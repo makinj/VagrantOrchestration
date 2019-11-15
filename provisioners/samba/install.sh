e756f4fbeb58071c394c7dbb934b99050922fc17
Joshua Makinen <makinj64@gmail.com>
2019-11-14 21:31:01 -0800

    fixes the install script

--- provisioners/samba/install.sh
+++ provisioners/samba/install.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install samba
 configtar="$HOME/configs/samba/config.tar.gz"
 
 if [ -f "$configtar" ]; then
-  tar -xzf "$configtar" -C /
+  sudo tar -xzf "$configtar" -C /
 fi
 
 sudo cp $HOME/configs/samba/smb.conf /etc/samba/smb.conf
