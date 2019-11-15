8fc0d04a485021570c38001a0d646f446c649e2f
Joshua Makinen <makinj64@gmail.com>
2019-11-14 21:49:03 -0800

    tries to get password db import/export working

--- scripts/samba/backup.sh
+++ scripts/samba/backup.sh
@@ -7,7 +7,7 @@ nonce="$(head -c 10 /dev/urandom | base32)"
 vagrant ssh -c "sudo pdbedit -e smbpasswd:/tmp/users.backup.tdb;"
 vagrant ssh -c "tar -C  / -cf '/tmp/config-$nonce.tar' \"/etc/samba/\";"
 vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/tmp/users.backup.tdb\";"
-vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/var/lib/samba/\";"
+#vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/var/lib/samba/\";"
 vagrant ssh -c "gzip '/tmp/config-$nonce.tar';"
 vagrant scp "default:/tmp/config-$nonce.tar.gz" "$1"
 
