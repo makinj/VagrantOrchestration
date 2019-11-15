36e9b90ab16662d8d706978ebcfe40e82fbe31ef
Joshua Makinen <makinj64@gmail.com>
2019-11-14 21:10:21 -0800

    adds to samba impl

--- provisioners/samba/install.sh
+++ provisioners/samba/install.sh
@@ -2,4 +2,11 @@
 
 sudo apt-get -y install samba
 
+
+configtar="$HOME/configs/smb/config.tar.gz"
+
+if [ -f "$configtar" ]; then
+  tar -xzf "$configtar" -C /
+fi
+
 sudo cp $HOME/configs/samba/smb.conf /etc/samba/smb.conf
--- scripts/samba/backup.sh
+++ scripts/samba/backup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 nonce="$(head -c 10 /dev/urandom | base32)"
-vagrant ssh -c "tar -C  / -cf '/tmp/config-$nonce.tar' \"/var/lib/samba/\";"
-vagrant ssh -c "tar -C  / -rf '/tmp/config-$nonce.tar' \"/etc/samba/\";"
+vagrant ssh -c "tar -C  / -cf '/tmp/config-$nonce.tar' \"/etc/samba/\";"
+vagrant ssh -c "sudo tar -C  / -rf '/tmp/config-$nonce.tar' \"/var/lib/samba/\";"
 vagrant ssh -c "gzip '/tmp/config-$nonce.tar';"
 vagrant scp "default:/tmp/config-$nonce.tar.gz" "$1"
 
