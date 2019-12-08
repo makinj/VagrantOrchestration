1c27862546430e48e34bf8906b4865a9494d0723
Joshua Makinen <makinj64@gmail.com>
2019-12-08 13:55:10 -0800

    random fixes and upgrades

--- provisioners/common/post_install.sh
+++ provisioners/common/post_install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 sudo usermod -G vboxsf -a vagrant
 sudo apt-get update
 sudo apt-get upgrade -y --with-new-pkgs
--- scripts/xfce/backup.sh
+++ scripts/xfce/backup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 nonce="$(head -c 10 /dev/urandom | base32)"
-vagrant ssh -c "tar -C / -cf '/tmp/config-$nonce.tar' \
+vagrant ssh -c "sudo tar -C / -cf '/tmp/config-$nonce.tar' \
  \"\$HOME/.config/user-dirs.dirs\" \
   \"\$HOME/.config/user-dirs.locale\" \
   \"\$HOME/.config/xfce4\" \
   \"\$HOME/.config\" \
   \"/usr/share/icons\" \
   \"/usr/share/themes\";"
-vagrant ssh -c "tar --no-recursion -rf '/tmp/config-$nonce.tar' \"\$HOME/.config/\";"
-vagrant ssh -c "gzip '/tmp/config-$nonce.tar'"
+vagrant ssh -c "sudo tar --no-recursion -rf '/tmp/config-$nonce.tar' \"\$HOME/.config/\";"
+vagrant ssh -c "sudo gzip '/tmp/config-$nonce.tar';sudo chown vagrant:vagrant '/tmp/config-$nonce.tar.gz'"
 vagrant scp "default:/tmp/config-$nonce.tar.gz" "$1"
 
 
